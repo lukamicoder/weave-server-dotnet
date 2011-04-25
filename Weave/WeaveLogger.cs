716ee9ce01535ec4501e2c64f3645e49ff5c22e4
lukamicoder@gmail.com <lukamicoder@gmail.com@2c1c97ad-0405-12b7-cb00-5d0a79c822f1>
2011-04-25 02:31:58 +0000

    Added impersonation to WeaveLogging
    git-svn-id: https://weave-server-dotnet.googlecode.com/svn/trunk@54 2c1c97ad-0405-12b7-cb00-5d0a79c822f1

--- Weave/WeaveLogger.cs
+++ Weave/WeaveLogger.cs
@@ -18,39 +18,67 @@
  * Boston, MA 02111-1307, USA.
  */
 
+using System;
+using System.Configuration;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 namespace Weave {
     enum LogType {
         Error = 0,
         Information = 1,
         Warning = 2,
+        Debug = 3,
     }
 
     static class WeaveLogger {
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
         public static void WriteMessage(string msg, LogType type) {
             const string appName = "Weave Server";
             if (!EventLog.SourceExists(appName)) {
                 EventLog.CreateEventSource(appName, "Application");
             }
 
-            using (EventLog eventLog = new EventLog()) {
-                eventLog.Source = appName;
-                switch (type) {
-                    case LogType.Error:
-                        StackTrace stackTrace = new StackTrace();
-                        StackFrame stackFrame = stackTrace.GetFrame(1);
-                        MethodBase methodBase = stackFrame.GetMethod();
-                        msg = "(" + methodBase.ReflectedType.Name + "." + methodBase.Name + ") " + msg;
-                        eventLog.WriteEntry(msg, EventLogEntryType.Error);
-                        break;
-                    case LogType.Warning:
-                        eventLog.WriteEntry(msg, EventLogEntryType.Warning);
-                        break;
-                    case LogType.Information:
-                        eventLog.WriteEntry(msg, EventLogEntryType.Information);
-                        break;
+            IntPtr token = IntPtr.Zero;
+            LogonUser(ConfigurationManager.AppSettings["LoggingUser.UserName"],
+                      ConfigurationManager.AppSettings["LoggingUser.Domain"],
+                      ConfigurationManager.AppSettings["LoggingUser.Password"], 2, 0, ref token);
+
+            if (token == IntPtr.Zero) {
+                return;
+            }
+
+            WindowsIdentity identity = new WindowsIdentity(token);
+            WindowsImpersonationContext impersonationContext = null;
+
+            try {
+                impersonationContext = identity.Impersonate();
+
+                using (EventLog eventLog = new EventLog()) {
+                    eventLog.Source = appName;
+                    switch (type) {
+                        case LogType.Error:
+                            StackTrace stackTrace = new StackTrace();
+                            StackFrame stackFrame = stackTrace.GetFrame(1);
+                            MethodBase methodBase = stackFrame.GetMethod();
+                            msg = "(" + methodBase.ReflectedType.Name + "." + methodBase.Name + ") " + msg;
+                            eventLog.WriteEntry(msg, EventLogEntryType.Error);
+                            break;
+                        case LogType.Warning:
+                            eventLog.WriteEntry(msg, EventLogEntryType.Warning);
+                            break;
+                        case LogType.Information:
+                            eventLog.WriteEntry(msg, EventLogEntryType.Information);
+                            break;
+                    }
+                }
+            } finally {
+                if (impersonationContext != null) {
+                    impersonationContext.Undo();
                 }
             }
         }
