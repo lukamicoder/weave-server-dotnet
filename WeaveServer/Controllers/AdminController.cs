4853f85d18fad6873abc58df25cbd8ba45cc7f9c
lukamicoder <lukamicoder@2c1c97ad-0405-12b7-cb00-5d0a79c822f1>
2012-02-21 21:11:33 +0000

    Updated license text
    git-svn-id: https://weave-server-dotnet.googlecode.com/svn/trunk@100 2c1c97ad-0405-12b7-cb00-5d0a79c822f1

--- WeaveServer/Controllers/AdminController.cs
+++ WeaveServer/Controllers/AdminController.cs
@@ -1,22 +1,22 @@
-﻿/* Copyright (C) 2011 Karoly Lukacs <lukamicoder@gmail.com>
- *
- * Based on code created by Mozilla Labs.
- * 
- * This is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this software; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
+﻿/* 
+Weave Server.NET <http://code.google.com/p/weave-server-dotnet/>
+Copyright (C) 2012 Karoly Lukacs
+
+Based on code created by Mozilla Labs.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 using System;
 using System.Web;
